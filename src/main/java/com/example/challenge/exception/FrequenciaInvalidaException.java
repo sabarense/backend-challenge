4cd821e524f0c946d7d3d0f0cbc6f9ecd21e556c
Yan Sabarense <sabarensey@gmail.com>
2025-02-05 20:46:38 -0300

    [feat] FrequenciaInvalidaException

---
+++ src/main/java/com/example/challenge/exception/FrequenciaInvalidaException.java
@@ -0,0 +1,7 @@
+package com.example.challenge.exception;
+
+public class FrequenciaInvalidaException extends RuntimeException {
+    public FrequenciaInvalidaException(String message) {
+        super(message);
+    }
+}
