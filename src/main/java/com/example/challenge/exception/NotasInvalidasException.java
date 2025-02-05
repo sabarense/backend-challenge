d582c184a77983dff44d36f2b55ed46d4345fc89
Yan Sabarense <sabarensey@gmail.com>
2025-02-05 20:46:53 -0300

    [feat] NotasInvalidasException

---
+++ src/main/java/com/example/challenge/exception/NotasInvalidasException.java
@@ -0,0 +1,7 @@
+package com.example.challenge.exception;
+
+public class NotasInvalidasException extends RuntimeException {
+    public NotasInvalidasException(String message) {
+        super(message);
+    }
+}
